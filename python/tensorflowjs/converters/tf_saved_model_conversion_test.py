9572bc138c0e915a6b174270fc088299d001d815
Ping Yu <4018+pyu10055@users.noreply.github.com>
2018-03-26 14:07:23 -0700

    more formatting change

--- python/tensorflowjs/converters/tf_saved_model_conversion_test.py
+++ python/tensorflowjs/converters/tf_saved_model_conversion_test.py
@@ -29,72 +29,72 @@ SAVED_MODEL_DIR = 'saved_model'
 
 
 class ConvertTest(unittest.TestCase):
-    def setUp(self):
-        super(ConvertTest, self).setUp()
-        self._tmp_dir = tempfile.mkdtemp()
+  def setUp(self):
+    super(ConvertTest, self).setUp()
+    self._tmp_dir = tempfile.mkdtemp()
 
-    def tearDown(self):
-        if os.path.isdir(self._tmp_dir):
-            shutil.rmtree(self._tmp_dir)
-        super(ConvertTest, self).tearDown()
+  def tearDown(self):
+    if os.path.isdir(self._tmp_dir):
+      shutil.rmtree(self._tmp_dir)
+    super(ConvertTest, self).tearDown()
 
-    def create_saved_model(self):
-        x = tf.constant([[37.0, -23.0], [1.0, 4.0]])
-        w = tf.Variable(tf.random_uniform([2, 2]))
-        y = tf.matmul(x, w)
-        tf.nn.softmax(y)
-        init_op = w.initializer
+  def create_saved_model(self):
+    x = tf.constant([[37.0, -23.0], [1.0, 4.0]])
+    w = tf.Variable(tf.random_uniform([2, 2]))
+    y = tf.matmul(x, w)
+    tf.nn.softmax(y)
+    init_op = w.initializer
 
-        # Create a builder
-        builder = tf.saved_model.builder.SavedModelBuilder(
-            os.path.join(self._tmp_dir, SAVED_MODEL_DIR))
+    # Create a builder
+    builder = tf.saved_model.builder.SavedModelBuilder(
+        os.path.join(self._tmp_dir, SAVED_MODEL_DIR))
 
-        with tf.Session() as sess:
-            # Run the initializer on `w`.
-            sess.run(init_op)
+    with tf.Session() as sess:
+      # Run the initializer on `w`.
+      sess.run(init_op)
 
-            builder.add_meta_graph_and_variables(
-                sess, [tf.saved_model.tag_constants.SERVING],
-                signature_def_map=None,
-                assets_collection=None)
+      builder.add_meta_graph_and_variables(
+          sess, [tf.saved_model.tag_constants.SERVING],
+          signature_def_map=None,
+          assets_collection=None)
 
-        builder.save()
+    builder.save()
 
-    def test_convert_saved_model(self):
-        self.create_saved_model()
+  def test_convert_saved_model(self):
+    self.create_saved_model()
 
-        tf_saved_model_conversion.convert_tf_saved_model(
-            os.path.join(self._tmp_dir, SAVED_MODEL_DIR),
-            'serve',
-            'Softmax',
-            os.path.join(self._tmp_dir, SAVED_MODEL_DIR),
-        )
+    tf_saved_model_conversion.convert_tf_saved_model(
+        os.path.join(self._tmp_dir, SAVED_MODEL_DIR),
+        'serve',
+        'Softmax',
+        os.path.join(self._tmp_dir, SAVED_MODEL_DIR),
+    )
 
-        weights = [{
-            'paths': ['group1-shard1of1'],
-            'weights': [{
-                'shape': [2, 2],
-                'name': 'Softmax',
-                'dtype': 'float32'
-            }]
+    weights = [{
+        'paths': ['group1-shard1of1'],
+        'weights': [{
+            'shape': [2, 2],
+            'name': 'Softmax',
+            'dtype': 'float32'
         }]
-        # Load the saved weights as a JSON string.
-        weights_manifest = open(
-            os.path.join(self._tmp_dir, SAVED_MODEL_DIR,
-                         'weights_manifest.json'), 'rt')
-        output_json = json.load(weights_manifest)
-        weights_manifest.close()
-        self.assertEqual(output_json, weights)
+    }]
+    # Load the saved weights as a JSON string.
+    weights_manifest = open(
+        os.path.join(self._tmp_dir, SAVED_MODEL_DIR,
+                     'weights_manifest.json'), 'rt')
+    output_json = json.load(weights_manifest)
+    weights_manifest.close()
+    self.assertEqual(output_json, weights)
 
-        # Check the content of the output directory.
-        self.assertTrue(
-            glob.glob(
-                os.path.join(self._tmp_dir, SAVED_MODEL_DIR,
-                             'tensorflowjs_model.pb')))
-        self.assertTrue(
-            glob.glob(
-                os.path.join(self._tmp_dir, SAVED_MODEL_DIR, 'group*-*')))
+    # Check the content of the output directory.
+    self.assertTrue(
+        glob.glob(
+            os.path.join(self._tmp_dir, SAVED_MODEL_DIR,
+                         'tensorflowjs_model.pb')))
+    self.assertTrue(
+        glob.glob(
+            os.path.join(self._tmp_dir, SAVED_MODEL_DIR, 'group*-*')))
 
 
 if __name__ == '__main__':
-    unittest.main()
+  unittest.main()
