ad3b51cdf75a9e0cf5c2cda537e5226167b95cdf
Ping Yu <4018+pyu10055@users.noreply.github.com>
2018-05-18 13:29:14 -0700

    use commonjs for compiled_api and import the namespace properly

--- src/executor/frozen_model_test.ts
+++ src/executor/frozen_model_test.ts
@@ -94,7 +94,7 @@ describe('Model', () => {
   });
 
   describe('eval', () => {
-    it('should generate the output', async () => {
+    fit('should generate the output', async () => {
       await model.load();
       const input = tfc.tensor1d([1], 'int32');
       const output = model.execute({'Input': input}, 'Add');
