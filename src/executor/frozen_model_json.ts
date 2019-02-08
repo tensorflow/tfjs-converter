c623e7b7b1f18d3124c3494079968dd6b9979604
Ping Yu <4018+pyu10055@users.noreply.github.com>
2019-02-07 22:23:59 -0800

    remove the accept headers (#287)
    * remove the accpept headers
    
    * update the test to mock httpBrowserRequest instead of fetch
    
    * fixed failing tests

--- src/executor/frozen_model_json.ts
+++ src/executor/frozen_model_json.ts
@@ -73,7 +73,7 @@ export class FrozenModel implements tfc.InferenceModel {
 
   private findIOHandler() {
     const path = this.modelUrl;
-    if (this.requestOption) {
+    if (this.requestOption || this.weightPrefix) {
       this.handler = tfc.io.browserHTTPRequest(
           path, this.requestOption, this.weightPrefix);
     } else {
