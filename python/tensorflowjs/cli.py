e944f24fd8b5be2bbc7ae26eff6d9124a66fca6c
Ping Yu <4018+pyu10055@users.noreply.github.com>
2019-06-28 16:23:02 -0700

    fixed pylint errors and added tests

--- python/tensorflowjs/cli.py
+++ python/tensorflowjs/cli.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 # ==============================================================================
-"""Interactive command line tool for building tensorflow.js conversion command."""
+"""Interactive command line tool for tensorflow.js model conversion."""
 
 from __future__ import print_function, unicode_literals
 
@@ -23,15 +23,10 @@ from PyInquirer import prompt
 from examples import custom_style_3
 from tensorflow.python.saved_model.loader_impl import parse_saved_model
 
-'''regex for recognizing valid url for TFHub module.'''
+# regex for recognizing valid url for TFHub module.
 URL_REGEX = re.compile(
-    r'^(?:http|ftp)s?://'  # http:// or https://
-    # domain...
-    r'(?:(?:[A-Z0-9](?:[A-Z0-9-]{0,61}[A-Z0-9])?\.)+(?:[A-Z]{2,6}\.?|[A-Z0-9-]{2,}\.?)|'
-    r'localhost|'  # localhost...
-    r'\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3})'  # ...or ip
-    r'(?::\d+)?'  # optional port
-    r'(?:/?|[/?]\S+)$', re.IGNORECASE)
+    # http:// or https://
+    r'^(?:http)s?://', re.IGNORECASE)
 
 
 def quantization_type(value):
@@ -57,8 +52,11 @@ def of_values(answers, key, values):
     key: question key.
     values: List of values to check from.
   """
-  value = answers[key]
-  return value in values
+  try:
+    value = answers[key]
+    return value in values
+  except KeyError:
+    return False
 
 
 def input_path_message(answers):
@@ -70,7 +68,7 @@ def input_path_message(answers):
   answer = answers['input_format']
   if answer == 'keras':
     return 'What is the path of input HDF5 file?'
-  elif answer == 'tfhub':
+  elif answer == 'tf_hub':
     return 'What is the TFHub module URL?'
   else:
     return 'What is the directory that contains the model?'
@@ -82,34 +80,31 @@ def validate_input_path(value, input_format):
     value: input path of the model.
     input_format: model format string.
   """
-  if not os.path.exists(value):
+  value = os.path.expanduser(value)
+  if input_format == 'tf_hub':
+    if re.match(URL_REGEX, value) is None:
+      return 'This is not an valid url for TFHub module: %s' % value
+  elif not os.path.exists(value):
     return 'Nonexistent path for the model: %s' % value
   if input_format in ['keras_saved_model', 'tf_saved_model']:
     if not os.path.isdir(value):
       return 'The path provided is not a directory: %s' % value
     if not any(fname.endswith('.pb') for fname in os.listdir(value)):
       return 'This is an invalid saved model directory: %s' % value
-  if input_format == 'tfhub':
-    if re.match(URL_REGEX, value) is None:
-      return 'This is not an valid url for TFHub module: %s' % value
   if input_format == 'tfjs_layers_model':
     if not os.path.isfile(value):
       return 'The path provided is not a file: %s' % value
-    if not os.path.exists(value + '/model.json'):
-      return 'This is not a valid directory for layers model: %s' % value
+  if input_format == 'keras':
+    if not os.path.isfile(value):
+      return 'The path provided is not a file: %s' % value
   return True
 
 
 def validate_output_path(value):
+  value = os.path.expanduser(value)
   if os.path.exists(value):
     return 'The output path already exists: %s' % value
   return True
-
-
-def default_signature_name(answers):
-  if of_values(answers, 'input_format', ['tf_saved_model']):
-    return 'serving_default'
-  return ''
 
 
 def generate_command(options, params):
@@ -117,45 +112,70 @@ def generate_command(options, params):
   args = 'tensorflowjs_converter'
   not_param_list = ['input_path', 'output_path']
   no_false_param = ['split_weights_by_layer', 'skip_op_check']
-  for key, value in params.items():
+  for key, value in sorted(params.items()):
     if key not in not_param_list and value is not None:
-      if key not in no_false_param or value is not False:
+      if key in no_false_param:
+        if value is True:
+          args += ' --%s' % (key)
+      else:
         args += ' --%s=%s' % (key, value)
 
   args += ' %s %s' % (params['input_path'], params['output_path'])
   return args
 
+
+def available_output_formats(answers):
+  input_format = answers['input_format']
+  if input_format == 'keras_saved_model':
+    return ['tfjs_graph_model', 'tfjs_layers_model']
+  if input_format == 'tfjs_layers_model':
+    return ['keras', 'tfjs_graph_model']
+  return []
+
 def available_tags(answers):
-  saved_model = parse_saved_model(answers['input_path'])
-  tags = []
-  for meta_graph in saved_model.meta_graphs:
-    tags.append(",".join(meta_graph.meta_info_def.tags))
-  return tags
+  if answers['input_format'] == 'tf_saved_model':
+    saved_model = parse_saved_model(answers['input_path'])
+    tags = []
+    for meta_graph in saved_model.meta_graphs:
+      tags.append(",".join(meta_graph.meta_info_def.tags))
+    return tags
+  return []
+
 
 def available_signature_names(answers):
-  path = answers['input_path']
-  tags = answers['saved_model_tags']
-  saved_model = parse_saved_model(path)
-  for meta_graph in saved_model.meta_graphs:
-    if tags == ",".join(meta_graph.meta_info_def.tags):
-      return meta_graph.signature_def.keys()
+  if answers['input_format'] == 'tf_saved_model':
+    path = answers['input_path']
+    tags = answers['saved_model_tags']
+    saved_model = parse_saved_model(path)
+    for meta_graph in saved_model.meta_graphs:
+      if tags == ",".join(meta_graph.meta_info_def.tags):
+        return meta_graph.signature_def.keys()
   return []
+
 
 def main():
   print('Weclome to TensorFlow.js converter.')
 
-  input_format = [
+  formats = [
       {
           'type': 'list',
           'name': 'input_format',
           'message': 'What is your input format?',
           'choices': ['keras', 'keras_saved_model',
-                      'tf_saved_model', 'tf_hub', 'tfjs_layers_model',
-                      'tensorflowjs']
+                      'tf_saved_model', 'tf_hub', 'tfjs_layers_model']
+      },
+      {
+          'type': 'list',
+          'name': 'output_format',
+          'message': 'What is your output format?',
+          'choices': available_output_formats,
+          'when': lambda answers: of_values(answers, 'input_format',
+                                            ['keras_saved_model',
+                                             'tfjs_layers_model'])
       }
   ]
 
-  options = prompt(input_format, style=custom_style_3)
+  options = prompt(formats, style=custom_style_3)
   message = input_path_message(options)
 
   questions = [
@@ -163,14 +183,16 @@ def main():
           'type': 'input',
           'name': 'input_path',
           'message': message,
-          'validate': lambda value: validate_input_path(value, options['input_format'])
+          'filter': os.path.expanduser,
+          'validate': lambda value: validate_input_path(
+              value, options['input_format'])
       },
       {
           'type': 'list',
           'name': 'saved_model_tags',
           'choices': available_tags,
           'message': 'What is tags for the saved model?',
-          'when': lambda answers: of_values(options, 'input_format',
+          'when': lambda answers: of_values(answers, 'input_format',
                                             ['tf_saved_model'])
       },
       {
@@ -178,51 +200,60 @@ def main():
           'name': 'signature_name',
           'message': 'What is signature name of the model?',
           'choices': available_signature_names,
-          'when': lambda answers: of_values(options, 'input_format',
+          'when': lambda answers: of_values(answers, 'input_format',
                                             ['tf_saved_model'])
       },
       {
           'type': 'list',
           'name': 'quantization_bytes',
           'message': 'Do you want to compress the model? '
-          '(this will decrease the model precision.)',
+                     '(this will decrease the model precision.)',
           'choices': ['No compression',
                       'compress weights to 1/2 the size',
                       'compress weights to 1/4 the size'],
           'filter': quantization_type
       },
       {
+          'type': 'input',
+          'name': 'weight_shard_size_byte',
+          'message': 'Please enter shard size (in bytes) of the weight files?',
+          'default': str(4 * 1024 * 1024),
+          'when': lambda answers: of_values(answers, 'output_format',
+                                            ['tfjs_layers_model'])
+      },
+      {
           'type': 'confirm',
           'name': 'split_weights_by_layer',
           'message': 'Do you want to split weights by layers?',
           'default': False,
-          'when': lambda answers: of_values(options, 'input_format',
-                                            ['tf_layers_model'])
+          'when': lambda answers: of_values(answers, 'input_format',
+                                            ['tfjs_layers_model'])
       },
       {
           'type': 'confirm',
           'name': 'skip_op_check',
           'message': 'Do you want to skip op validation?',
           'default': False,
-          'when': lambda answers: of_values(options, 'input_format',
-                                            ['tf_saved_model', 'tfhub'])
+          'when': lambda answers: of_values(answers, 'input_format',
+                                            ['tf_saved_model', 'tf_hub'])
       },
       {
           'type': 'confirm',
           'name': 'strip_debug_ops',
           'message': 'Do you want to strip debug ops?',
           'default': False,
-          'when': lambda answers: of_values(options, 'input_format',
-                                            ['tf_saved_model', 'tfhub'])
+          'when': lambda answers: of_values(answers, 'input_format',
+                                            ['tf_saved_model', 'tf_hub'])
       },
       {
           'type': 'input',
           'name': 'output_path',
           'message': 'Which directory do you want save the converted model?',
-          'validate': lambda value: validate_output_path(value)
+          'filter': os.path.expanduser,
+          'validate': validate_output_path
       }
   ]
-  params = prompt(questions, style=custom_style_3)
+  params = prompt(questions, options, style=custom_style_3)
 
   command = generate_command(options, params)
   print(command)
---
+++ python/tensorflowjs/cli_test.py
@@ -0,0 +1,194 @@
+# Copyright 2019 Google LLC
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+# ==============================================================================
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import unittest
+import tempfile
+import os
+import shutil
+import tensorflow as tf
+from tensorflow.python.eager import def_function
+from tensorflow.python.ops import variables
+from tensorflow.python.training.tracking import tracking
+from tensorflow.python.saved_model.save import save
+
+from tensorflowjs import cli
+
+SAVED_MODEL_DIR = 'saved_model'
+HD5_FILE_NAME = 'test.HD5'
+
+
+class CliTest(unittest.TestCase):
+  def setUp(self):
+    self._tmp_dir = tempfile.mkdtemp()
+    super(CliTest, self).setUp()
+
+  def tearDown(self):
+    if os.path.isdir(self._tmp_dir):
+      shutil.rmtree(self._tmp_dir)
+    super(CliTest, self).tearDown()
+
+  def _create_hd5_file(self):
+    filename = os.path.join(self._tmp_dir, 'test.HD5')
+    open(filename, 'a').close()
+
+  def _create_saved_model(self):
+    """Test a basic model with functions to make sure functions are inlined."""
+    input_data = tf.constant(1., shape=[1])
+    root = tracking.AutoTrackable()
+    root.v1 = variables.Variable(3.)
+    root.v2 = variables.Variable(2.)
+    root.f = def_function.function(lambda x: root.v1 * root.v2 * x)
+    to_save = root.f.get_concrete_function(input_data)
+
+    save_dir = os.path.join(self._tmp_dir, SAVED_MODEL_DIR)
+    save(root, save_dir, to_save)
+
+  def testQuantizationType(self):
+    self.assertEqual(2, cli.quantization_type('1/2'))
+    self.assertEqual(1, cli.quantization_type('1/4'))
+    self.assertEqual(None, cli.quantization_type('1'))
+
+  def testOfValues(self):
+    answers = {'input_path': 'abc', 'input_format': '123'}
+    self.assertEqual(True, cli.of_values(answers, 'input_path', ['abc']))
+    self.assertEqual(False, cli.of_values(answers, 'input_path', ['abd']))
+    self.assertEqual(False, cli.of_values(answers, 'input_format2', ['abc']))
+
+  def testInputPathMessage(self):
+    answers = {'input_format': 'keras'}
+    self.assertEqual("What is the path of input HDF5 file?",
+                     cli.input_path_message(answers))
+
+    answers = {'input_format': 'tf_hub'}
+    self.assertEqual("What is the TFHub module URL?",
+                     cli.input_path_message(answers))
+
+    answers = {'input_format': 'tf_saved_model'}
+    self.assertEqual("What is the directory that contains the model?",
+                     cli.input_path_message(answers))
+
+  def testValidateInputPathForTFHub(self):
+    self.assertNotEqual(True, cli.validate_input_path(self._tmp_dir, 'tf_hub'))
+    self.assertEqual(True,
+                     cli.validate_input_path("https://tfhub.dev/mobilenet",
+                                             'tf_hub'))
+
+  def testValidateInputPathForSavedModel(self):
+    self.assertNotEqual(True, cli.validate_input_path(
+        self._tmp_dir, 'tf_saved_model'))
+    self._create_saved_model()
+    save_dir = os.path.join(self._tmp_dir, SAVED_MODEL_DIR)
+    self.assertEqual(True, cli.validate_input_path(
+        save_dir, 'tf_saved_model'))
+
+  def testValidateInputPathForKerasSavedModel(self):
+    self.assertNotEqual(True, cli.validate_input_path(
+        self._tmp_dir, 'keras_saved_model'))
+    self._create_saved_model()
+    save_dir = os.path.join(self._tmp_dir, SAVED_MODEL_DIR)
+    self.assertEqual(True, cli.validate_input_path(
+        save_dir, 'keras_saved_model'))
+
+  def testValidateInputPathForKerasModel(self):
+    self.assertNotEqual(True, cli.validate_input_path(self._tmp_dir, 'keras'))
+    self._create_hd5_file()
+    save_dir = os.path.join(self._tmp_dir, HD5_FILE_NAME)
+    self.assertEqual(True, cli.validate_input_path(
+        save_dir, 'keras'))
+
+  def testValidateOutputPath(self):
+    self.assertNotEqual(True, cli.validate_output_path(self._tmp_dir))
+    output_dir = os.path.join(self._tmp_dir, 'test')
+    self.assertEqual(True, cli.validate_output_path(output_dir))
+
+  def testAvailableTags(self):
+    self._create_saved_model()
+    save_dir = os.path.join(self._tmp_dir, SAVED_MODEL_DIR)
+    self.assertEqual(['serve'], cli.available_tags(
+        {'input_path': save_dir,
+         'input_format': 'tf_saved_model'}))
+
+  def testAvailableSignatureNames(self):
+    self._create_saved_model()
+    save_dir = os.path.join(self._tmp_dir, SAVED_MODEL_DIR)
+    self.assertEqual(['__saved_model_init_op', 'serving_default'],
+                     list(cli.available_signature_names(
+                         {'input_path': save_dir,
+                          'input_format': 'tf_saved_model',
+                          'saved_model_tags': 'serve'})))
+
+  def testGenerateCommandForSavedModel(self):
+    options = {'input_format': 'tf_saved_model'}
+    params = {'input_path': 'tmp/saved_model',
+              'saved_model_tags': 'test',
+              'signature_name': 'test_default',
+              'quantization_bytes': 2,
+              'skip_op_check': False,
+              'strip_debug_ops': True,
+              'output_path': 'tmp/web_model'}
+
+    self.assertEqual('tensorflowjs_converter --input_format=tf_saved_model ' +
+                     '--quantization_bytes=2 --saved_model_tags=test ' +
+                     '--signature_name=test_default --strip_debug_ops=True ' +
+                     'tmp/saved_model tmp/web_model',
+                     cli.generate_command(options, params))
+
+  def testGenerateCommandForKerasSavedModel(self):
+    options = {'input_format': 'tf_keras_saved_model',
+               'output_format': 'tfjs_layers_model'}
+    params = {'input_path': 'tmp/saved_model',
+              'saved_model_tags': 'test',
+              'signature_name': 'test_default',
+              'quantization_bytes': 1,
+              'skip_op_check': True,
+              'strip_debug_ops': False,
+              'output_path': 'tmp/web_model'}
+
+    self.assertEqual('tensorflowjs_converter ' +
+                     '--input_format=tf_keras_saved_model ' +
+                     '--output_format=tfjs_layers_model ' +
+                     '--quantization_bytes=1 --saved_model_tags=test ' +
+                     '--signature_name=test_default --skip_op_check ' +
+                     '--strip_debug_ops=False tmp/saved_model tmp/web_model',
+                     cli.generate_command(options, params))
+
+  def testGenerateCommandForKerasModel(self):
+    options = {'input_format': 'keras'}
+    params = {'input_path': 'tmp/model.HD5',
+              'quantization_bytes': 1,
+              'output_path': 'tmp/web_model'}
+
+    self.assertEqual('tensorflowjs_converter --input_format=keras ' +
+                     '--quantization_bytes=1 tmp/model.HD5 tmp/web_model',
+                     cli.generate_command(options, params))
+
+  def testGenerateCommandForLayerModel(self):
+    options = {'input_format': 'tfjs_layers_model',
+               'output_format': 'keras'}
+    params = {'input_path': 'tmp/model.json',
+              'quantization_bytes': 1,
+              'output_path': 'tmp/web_model'}
+
+    self.assertEqual('tensorflowjs_converter ' +
+                     '--input_format=tfjs_layers_model ' +
+                     '--output_format=keras ' +
+                     '--quantization_bytes=1 tmp/model.json tmp/web_model',
+                     cli.generate_command(options, params))
+
+if __name__ == '__main__':
+  unittest.main()
